a2a57464c23a7b6e2a1403b560c01ea95c57e709
Kwame8919 <101393080+Kwame8919@users.noreply.github.com>
2022-11-13 20:48:52 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is the state class"""
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
 import models
-from models.base_model import BaseModel, Base
 from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
@@ -13,15 +13,17 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
+
+    
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """Getter attribute in case of file storage"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+            return [city for city in models.storage.all(
+                City).values() if city.state_id == self.id]
